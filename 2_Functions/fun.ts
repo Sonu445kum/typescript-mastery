546a5728f709818fabc0b33376d2d204767fa7ee
Sonu445Kum <sonuroy1629@gmail.com>
2026-04-30 14:25:20 +0530

    Solved:arrow func

--- 2_Functions/fun.ts
+++ 2_Functions/fun.ts
@@ -6,7 +6,11 @@
 // console.log(`sum of Two Number:`,add(4,5));
 
 // 2. Function Type Inference 
-function greet(name:string){
-    return `Hello` + name;
-}
-console.log(greet("Sonu"));
+// function greet(name:string){
+//     return `Hello` + name;
+// }
+// console.log(greet("Sonu"));
+
+// 3. Arrow Functions
+const multiply = (a:number , b : number): number=>a*b;
+console.log(`Multiply :`,multiply(2,4));
